5008a79730d5ece016e393acbd8dd12a9ed738cb
Chelsiiy <151575298+Chelsiiy@users.noreply.github.com>
2024-06-18 15:10:48 -0700

    Update Login.js

--- Login.js
+++ Login.js
@@ -1,13 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import { StyleSheet, Text, TextInput, TouchableOpacity, View,Image } from 'react-native';
 
 export default function Login() {
   return (
     <View style={styles.container}>
-      <Text style={styles.firstTitle}>JOBIZZ</Text>
-      <Text style={styles.title}>WELCOME BACK</Text>
-      <Text style={styles.subtitle}>LET'S LOGIN. APPLY TO JOBS</Text>
+      <Text style={styles.title}>Jobizz</Text>
+      <Text style={styles.welcome}>Welcome Back</Text>
+      <Text style={styles.apply}>Let's login. Apply to jobs!</Text>
       <TextInput
         style={styles.input}
         placeholder="Username"
@@ -20,7 +20,13 @@ export default function Login() {
       <TouchableOpacity style={styles.button} onPress={() => alert('Login button pressed')}>
         <Text style={styles.buttonText}>Login</Text>
       </TouchableOpacity>
-      <Image source={require('./assets/104490_apple_icon.png')} style={styles.Icons}/>
+      <View style={styles.lineContainer}>
+        <View style={styles.line} />
+        <Text style={styles.orText}>Or continue with</Text>
+        <View style={styles.line} />
+      </View>
+      <Image source={require('./assets/104490_apple_icon.png')} style={styles.icons}/>
+      <Image source={require('./assets/2993685_brand_brands_google_logo_logos_icon.png')} style={styles.icons}/>
       <StatusBar style="auto" />
     </View>
   );
@@ -30,44 +36,64 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
+    alignItems: 'flex-start',
     justifyContent: 'flex-start',
-    padding: 20,
-    paddingTop: 80, 
-  },
-  firstTitle: {
-    fontSize: 25,
-    color: 'blue',
-    marginBottom: 10,
-    fontWeight: 'bold',
+    padding: 16,
   },
   title: {
-    fontSize: 24,
+    fontSize: 22,
     marginBottom: 10,
-    textAlign: 'center',
-  },
-  subtitle: {
-    fontSize: 18,
-    marginBottom: 20,
-    fontWeight: '300',
-    textAlign: 'center',
+    marginTop: 140,
+    marginLeft: 18,
+    fontWeight: 'bold',
+    color: '#5072A7',
   },
   input: {
-    width: '90', 
-    maxWidth: 400,
-    padding: 12,
-    marginVertical: 10,
+    width: 310,
+    padding: 10,
+    marginVertical: 8,
     borderWidth: 1,
     borderColor: '#ccc',
     borderRadius: 5,
+    marginBottom: 20,
+    marginLeft: 18,
+  },
+  welcome: {
+    fontSize: 25,
+    marginBottom: 5,
+    marginLeft: 18,
+    fontWeight: 'bold',
+  },
+  apply: {
+    marginLeft: 18,
+    marginBottom: 60,
+    color: 'grey',
+  },
+  lineContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginTop: 20,
+    marginBottom: 20,
+    alignSelf: 'stretch', 
+    marginHorizontal: 18, 
+  },
+  line: {
+    flex: 1,
+    height: 1,
+    backgroundColor: 'grey',
+  },
+  orText: {
+    marginHorizontal: 10,
+    color: 'grey',
   },
   button: {
     backgroundColor: 'blue',
     padding: 15,
     marginTop: 20,
-    width: '80%', 
+    width: 310,
     maxWidth: 400, 
     alignItems: 'center',
+    alignSelf: 'center',
     borderRadius: 5,
   },
   buttonText: {
@@ -75,9 +101,10 @@ const styles = StyleSheet.create({
     fontSize: 16,
     fontWeight: 'bold',
   },
-  Icons:{
-    width:50,
-    length:50,
+  icons:{
+    width:250,
+    length:250,
     borderRadius:10,
+    marginRight:20,
   }
 });
